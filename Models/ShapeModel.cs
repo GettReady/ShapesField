86ab000d76abfe7a9ea8362bdd7867d190b819f0
GettReady <eroxinand@gmail.com>
2023-01-23 10:27:13 +0300

    Added ShapeModel

---
+++ Models/ShapeModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ShapesField.Models
+{
+    public class ShapeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; } = "lightgray";
+        public int PositionX { get; set; } = 0;
+        public int PositionY { get; set; } = 0;
+
+        public ShapeModel() { }
+
+        public ShapeModel(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public ShapeModel(string name, string type, string color, int posX, int posY)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+            PositionX = posX;
+            PositionY = posY;
+        }
+    }
+}
