48621554644ef601b3d7e37e2f3dc64022544ace
GettReady <eroxinand@gmail.com>
2023-01-24 13:09:38 +0300

    Action-form component layout

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShapesFieldComponent } from './shapes-field/shapes-field.component';
+import { ActionFormComponent } from './action-form/action-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ShapesFieldComponent
+    ShapesFieldComponent,
+    ActionFormComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
