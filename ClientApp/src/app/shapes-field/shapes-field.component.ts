493418bcf99fcb4caced4d1598f031fa2c8352de
GettReady <eroxinand@gmail.com>
2023-02-02 23:56:09 +0300

    Svg element rendering moved to a separate class

--- ClientApp/src/app/shapes-field/shapes-field.component.ts
+++ ClientApp/src/app/shapes-field/shapes-field.component.ts
@@ -22,8 +22,8 @@ export class ShapesFieldComponent implements OnInit {
   shapes_container_id = "svg-container";
   offsetX!: number;
   offsetY!: number;
-  is_moving: boolean = false;
   moveHandler = (event: any) => { this.move(event) };
+  renderer = new SvgRenderer();
 
   constructor(private http: HttpClient,
     @Inject('BASE_URL') private baseUrl: string,
@@ -62,7 +62,7 @@ export class ShapesFieldComponent implements OnInit {
           this.shapes_array[id] = structuredClone(new_shape);
           let element = document.querySelector<HTMLElement>('#' + this.shapes_id_array[id]);
           if (new_shape.id == this.selected_shape?.id) {            
-            this.data.selectShape(this.shapes_array[id]);
+            this.data.selectShape(this.shapes_array[id]);            
           }
           element?.remove();
           this.appendShapeById(this.shapes_array[id], this.shapes_id_array[id]);
@@ -95,12 +95,12 @@ export class ShapesFieldComponent implements OnInit {
   appendShapeByIndex(shape: Shape, index: number) {
     let group_id = "group" + index;
     if (this.shapes_container)
-      this.drawSvgShape(shape, this.shapes_container, group_id, this.startMoving, this.stopMoving);
+      this.drawShape(shape, this.shapes_container, group_id);
   }
 
   appendShapeById(shape: Shape, id: string) {
     if (this.shapes_container)
-      this.drawSvgShape(shape, this.shapes_container, id, this.startMoving, this.stopMoving);
+      this.drawShape(shape, this.shapes_container, id);
   }
 
   appendShape(shape: Shape) {
@@ -109,59 +109,36 @@ export class ShapesFieldComponent implements OnInit {
     this.shapes_id_array.push(group_id);
 
     if (this.shapes_container)
-      this.drawSvgShape(shape, this.shapes_container, group_id, this.startMoving, this.stopMoving);
+      this.drawShape(shape, this.shapes_container, group_id);
   }
 
-  drawSvgShape(shape: Shape, shapes_container: HTMLElement, id: string, eventHandlerStart: Function, eventHandlerStop: Function) {
-    let shape_element = `<svg id="${id}" x="${shape.positionX}" y="${shape.positionY}" width="100px" height="100px">`;
-    switch (shape.type) {
-      case 'square':
-        shape_element += `<rect x="0" y="0" width="100" height="100" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
-        break;
-      case 'circle':
-        shape_element += `<circle cx="50" cy="50" r="50" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
-        break;
-      case 'triangle':
-        shape_element += `<polygon points="0 100 50 0 100 100" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
-        break;
-    }
-    shape_element += `<text x="50" y = "50" alignment-baseline="middle" text-anchor="middle" style="user-select: none" class="shape-name" > ${shape.name}</text>`;
-
-    shapes_container.insertAdjacentHTML('beforeend', shape_element);
-    document.querySelector('#' + id)!.addEventListener('mousedown', eventHandlerStart.bind(this));
-    document.querySelector('#' + id)!.addEventListener('mouseup', eventHandlerStop.bind(this));
+  drawShape(shape: Shape, shapes_container: HTMLElement, id: string) {
+    this.renderer.renderShape(shape, shapes_container, id);
+    document.querySelector('#' + id)!.addEventListener('mousedown', this.startMoving.bind(this));
+    document.querySelector('#' + id)!.addEventListener('mouseup', this.stopMoving.bind(this));
   }
 
   startMoving(event: any) {
     this.removeSelection();    
     this.addSelection(event.target.parentNode);    
-
-    this.offsetX = event.clientX - this.selected_element!.getBoundingClientRect().x;
-    this.offsetY = event.clientY - this.selected_element!.getBoundingClientRect().y;
-
+    this.renderer.setOffsets(event.clientX, event.clientY);
     document.addEventListener("mousemove", this.moveHandler);
-    this.selected_element!.classList.add('svg-shadow');
   }
 
   stopMoving(event: any) {
     document.removeEventListener("mousemove", this.moveHandler);
-
-    if (this.is_moving) {
-      if (this.selected_shape) {
-        this.selected_shape.positionX = Number(this.selected_element?.getAttribute('x'));
-        this.selected_shape.positionY = Number(this.selected_element?.getAttribute('y'));        
+    let new_pos = this.renderer.getShapePosition();
+    if (this.selected_shape) { 
+      if (this.selected_shape.positionX !== new_pos.x || this.selected_shape.positionY !== new_pos.y) {
+        this.selected_shape.positionX = new_pos.x;
+        this.selected_shape.positionY = new_pos.y;
         this.request.editShape(this.selected_shape).subscribe();
       }
-      this.is_moving = false;
     }
   }  
 
   move(event: any) {
-    this.selected_element!.setAttribute('x', "" + (event.clientX - this.offsetX));
-    this.selected_element!.setAttribute('y', "" + (event.clientY - 64 - this.offsetY));
-
-    if (!this.is_moving)
-      this.is_moving = true;
+    this.renderer.translateShape(event.clientX, event.clientY - 64);
   }
 
   deselect(event: any) {    
@@ -169,22 +146,74 @@ export class ShapesFieldComponent implements OnInit {
   }
 
   addSelection(parent_element: HTMLElement) {
-    this.selected_element = parent_element;
     this.selected_shape = this.shapes_array[this.shapes_id_array.indexOf(parent_element.id)];
-
     this.data.selectShape(this.selected_shape);
+    this.renderer.selectElement(parent_element);
+  }
+  
+  removeSelection() {
+    this.renderer.deselectElement();
+    this.selected_shape = undefined;
+    this.data.deselectShape();
+  }
+}
 
-    parent_element.children[0].classList.add('svg-stroke');
+export class SvgRenderer {
+  selected_element?: HTMLElement;
+  outer_selection_class = "svg-shadow";
+  inner_selection_class = "svg-stroke";
+  width = 100;
+  height = 100;
+  offset_x = 0;
+  offset_y = 0;
+
+  constructor() { }
+
+  public selectElement(element: HTMLElement) {
+    this.selected_element = element;
+    this.selected_element.classList.add(this.outer_selection_class);
+    this.selected_element.children[0].classList.add(this.inner_selection_class);
   }
 
-  removeSelection() {
-    if (this.selected_element) {      
-      this.selected_element!.classList.remove('svg-shadow');
-      this.selected_element!.children[0].classList.remove('svg-stroke');
-      this.selected_element = undefined;
-      this.selected_shape = undefined;
-
-      this.data.deselectShape();
+  public deselectElement() {
+    if (this.selected_element) {
+      this.selected_element.classList.remove(this.outer_selection_class);
+      this.selected_element.children[0].classList.remove(this.inner_selection_class);
     }
   }
+
+  public setOffsets(x: number, y: number) {
+    this.offset_x = x - this.selected_element!.getBoundingClientRect().x;
+    this.offset_y = y - this.selected_element!.getBoundingClientRect().y;
+  }
+
+  public translateShape(x: number, y: number) {
+    this.selected_element!.setAttribute('x', "" + (x - this.offset_x));
+    this.selected_element!.setAttribute('y', "" + (y - this.offset_y));
+  }
+
+  public getShapePosition(): {x: number, y:number} {
+    let new_pos_x = Number(this.selected_element?.getAttribute('x'));
+    let new_pos_y = Number(this.selected_element?.getAttribute('y'));
+
+    return { x: new_pos_x, y: new_pos_y };
+  }
+
+  public renderShape(shape: Shape, shapes_container: HTMLElement, id: string) {
+    let shape_element = `<svg id="${id}" x="${shape.positionX}" y="${shape.positionY}" width="${this.width}px" height="${this.height}px">`;
+    switch (shape.type) {
+      case 'square':
+        shape_element += `<rect x="0" y="0" width="${this.width}px" height="${this.height}px" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
+        break;
+      case 'circle':
+        shape_element += `<circle cx="${this.width * 0.5}" cy="${this.height * 0.5}" r="${this.width * 0.5}" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
+        break;
+      case 'triangle':
+        shape_element += `<polygon points="0 ${this.height} ${this.width * 0.5} 0 ${this.width} ${this.height}" fill="${shape.color}" stroke-width="1" stroke="rgba(0,0,0,0.5)" />`;
+        break;
+    }
+    shape_element += `<text x="${this.width * 0.5}" y = "${this.height * 0.5}" alignment-baseline="middle" text-anchor="middle" style="user-select: none" class="shape-name" > ${shape.name}</text>`;
+    shapes_container.insertAdjacentHTML('beforeend', shape_element);
+  }
+
 }
